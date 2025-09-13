a65c32efb45fc33d4ab8a377aec05a70b0870e4e
anish-kumar <anishkumarmynet389@gmail.com>
2025-09-13 18:47:32 +0530

    Kafka fallback

--- src/main/java/com/anish/journalApp/scheduler/UserScheduler.java
+++ src/main/java/com/anish/journalApp/scheduler/UserScheduler.java
@@ -53,7 +53,11 @@ public class UserScheduler {
                 }
                 if(mostFrequentSentiment != null){
                     SentimentData sentimentData = SentimentData.builder().email(user.getEmail()).sentiment("Sentiment for last 7 days "+ mostFrequentSentiment).build();
-                    kafkaTemplate.send("weekly_sentiments", sentimentData.getEmail(), sentimentData);
+                    try {
+                        kafkaTemplate.send("weekly_sentiments", sentimentData.getEmail(), sentimentData);
+                    } catch (Exception e) {
+                        mailService.sendMail(sentimentData.getEmail(), "Sentiment for previous week", sentimentData.getSentiment());
+                    }
                 }
             }
         }
@@ -64,4 +68,3 @@ public class UserScheduler {
         appCache.init();
     }
 }
-
